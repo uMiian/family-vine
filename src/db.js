67d0a7a12629801a1255b5ce9c2194106f9c76d4
Teeler <tyler73750@gmail.com>
2024-10-01 23:14:27 -0500

    Created new tables Locations and People, and made relations to media

--- src/db.js
+++ src/db.js
@@ -9,17 +9,54 @@ const sequelize = new Sequelize({
   logging: false,
 })
 
-// Create models (i.e. tables)
-const Media  = sequelize.define('Media', {
-  filepath: {
+/** Create models (i.e. tables)! */
+const Person = sequelize.define('Person', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+  }
+})
+
+const Location = sequelize.define('Location', {
+  name: {
     type: DataTypes.STRING,
     allowNull: false,
   }
 })
 
+const Media  = sequelize.define('Media', {
+  description: {
+    type: DataTypes.TEXT,
+  },
+  when: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  filepath: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+
+
+/** Create relations between models */
+const MediaPerson = sequelize.define('MediaPerson')
+
+Media.belongsTo(Person); // A Media item belongs to one Person
+Person.hasMany(Media);   // A Person can have taken multiple Media
+
+
+Person.belongsToMany(Media, { through: MediaPerson }); // A Person can be associated with many Media
+Media.belongsToMany(Person, { through: MediaPerson}); // A media can be associated with many Persons
+
+
 // Sync the database (i.e. create the tables if not already made)
 sequelize.sync({alter: true})
-  .then(() => console.log("Databse & tables created!"))
+  .then(() => console.log("Database & tables created!"))
   .catch(err => console.log("Error syncing database, ", err))
 
-module.exports = { sequelize, Media }
+module.exports = { sequelize, Media, Person, Location }
