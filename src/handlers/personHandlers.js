ec37fb6cbc696987d0027470dcfcc1b6faee5c59
ROMANT21 <tyler73750@gmail.com>
2024-10-10 23:51:47 -0500

    Created add and get all for person handlers

--- src/handlers/personHandlers.js
+++ src/handlers/personHandlers.js
@@ -6,6 +6,7 @@ export const setupPersonHandlers = (models) => {
   ipcMain.removeHandler(IPC_HANDLERS.PERSON_ADD);
   ipcMain.removeHandler(IPC_HANDLERS.PERSON_GET_ALL);
 
+  // Handler to create new person
   ipcMain.handle(IPC_HANDLERS.PERSON_ADD, async (context, personData) => {
     try {
       const newPerson = await models.Person.create(personData);
@@ -15,11 +16,17 @@ export const setupPersonHandlers = (models) => {
     }
   });
 
+  // Handler to get new people
   ipcMain.handle(IPC_HANDLERS.PERSON_GET_ALL, async (context) => {
+    console.log("Getting all people from the database!")
     try {
       return models.Person.findAll();
     } catch (error) {
       console.error("Error getting all people:", error);
     }
   })
+
+  // TODO: Handler to get specific person
+  
+  // TODO: Handler to delete person
 }
