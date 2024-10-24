abf5b532cf6ecf918d6ba76289b96b002f10a4f6
Khoi Pham <khoipham@Khois-MacBook-Pro.local>
2024-10-24 12:09:42 -0500

    home page updates

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './styles/home.css';
 
 function Home() {
-    // Create state 
+    const [showInputPage, setShowInputPage] = useState(false);
     const [newFamilyVineName, setNewFamilyVineName] = useState('');
 
     async function createFamilyVine(event) {
@@ -30,20 +28,29 @@ function Home() {
             </div>
             <div className="main-content">
                 <img src="/images/logo.png" alt="Family Vine Logo" className="logo" />
-                <h1>Family Vine</h1>
-                <p>V 1.0.0</p>
-                <div className="actions">
-                    <div className="action-item">
-                        <p>Create new Family Vine</p>
-                        <br />
-                        <p>New Family Vine Name</p><input onChange={(event) => {setNewFamilyVineName(event.target.value)}}/>
-                        <button className="action-button" onClick={createFamilyVine}>Create</button>
-                    </div>
-                    <div className="action-item">
-                        <p>Load Family Vine</p>
+                <h1><span>Family Vine</span></h1>
+                <p className = "version">V 1.0.0</p>
+                {!showInputPage ? (
+                    <div className="actions">
+                        <div className="action-item">
+                            <p className="options-text">Create new Family Vine</p>
+                            <button className="action-button" onClick={() => setShowInputPage(true)}>Create</button>
+                        </div>
+                        <div className="action-item">
+                            <p className="options-text">Load Family Vine</p>
                             <button className="action-button" onClick={loadFamilyVine}>Load</button>
+                        </div>
                     </div>
-                </div>
+                ) : (
+                    <div className="input-page">
+                        <p className="input-page-options">Enter Vine Name</p>
+                        <input onChange={(event) => { setNewFamilyVineName(event.target.value); }} />
+                        <div className="button-group">
+                            <button className="action-button" onClick={createFamilyVine}>Create</button>
+                            <button className="action-button" onClick={() => setShowInputPage(false)}>Back</button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
