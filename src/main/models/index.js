a4b1112257b71d63eafb7d8c5f527bf7fdd64519
Tyler R <tyler73750@gmail.com>
2024-12-17 18:31:11 -0600

    Created database services with accompanying tests

--- src/main/models/index.js
+++ src/main/models/index.js
@@ -4,14 +4,20 @@ import { definePerson } from './person.js';
 import { defineLocation }  from './location.js';
 import { defineRelationships } from './relationships.js';
 
-// Given a database filepath, connect to a database, define the models, and returnt the sequelize instance
+// Check if we are in testing
+const isTest = process.env.NODE_ENV === 'test';
+
+// Given a database filepath, connect to a database, define the models, and return the sequelize instance
 // NOTE: If the connection does not exist, it will create a new sqlite db file.
-export async function connectToDb(dbFilepath) {
+export async function createDbInstance(dbFilepath) {
 
   // Connect to the database (or create it if it doesn't exist)
   const sequelizeInstance = new Sequelize({
     dialect: 'sqlite',
-    storage: dbFilepath
+    storage: dbFilepath,
+
+    // If we are testing, disable logging
+    logging: isTest ? false : console.log,
   })
 
   // Make sure the connection is ok
@@ -32,7 +38,7 @@ export async function connectToDb(dbFilepath) {
 
   // Sync the models with the database
   try {
-    await sequelizeInstance.sync({ alter: true })
+    await sequelizeInstance.sync();
   } catch (error) {
     throw Error('Could not synchronize models with database:', error);
   }
--- src/main/services/DatabaseService.js
+++ src/main/services/DatabaseService.js
@@ -1,45 +1,102 @@
 import * as fs from 'fs/promises';
+import { createDbInstance } from '../models';
 
 // The current instance of the database
 let dbInstance = null;
+let currentDbFilepath = null;
 
 // Given a database filepath connect to an already existing database.
-async function connectToDb(dbFilepath) {
+export async function connectToDb(dbFilepath) {
   // Check to see if db exists
+  try {
+    await fs.access(dbFilepath);
+  } catch (error) {
+    throw new Error(`Could not find database at ${dbFilepath}:`, error);
+  }
 
   // Try to connect to db
-  
-  // Set the new instance
+  try {
+    dbInstance = await createDbInstance(dbFilepath);
+    currentDbFilepath = dbFilepath;
+  } catch (error) {
+    console.log(error);
+    throw new Error(`Could not connect to database at ${dbFilepath}:`, error);
+  }
 }
 
 
 // Given a database filepath, create and connect to a database
-async function createAndConnectToDb(dbFilpath) {
+export async function createAndConnectToDb(dbFilepath) {
   // Ensure there is not already a db with that name
+  try {
+    await fs.access(dbFilepath);
+    throw new Error(`Database already exists at ${dbFilepath}`);
+  } catch (error) {
+    // Don't raise an error if it is a 'file does not exist error'
+    // raise all other errors
+    if (error.code !== 'ENOENT') {
+      throw error;
+    }
+  }
 
   // Try to create and connect to db
-
-  // Set the new instance
+  try {
+    dbInstance = await createDbInstance(dbFilepath);
+    currentDbFilepath = dbFilepath;
+  } catch (error) {
+    throw new Error(`Could not create and connect to database at ${dbFilepath}:`, error);
+  }
 }
 
 // Disconnect from a database
-async function disconnectFromDb() {
-  // End the db connection
+export async function disconnectFromDb() {
+  if (!dbInstance & !currentDbFilepath) {
+    throw new Error('There is no database instance');
+  }
+
+  // Try to end the db connection
+  try {
+    await dbInstance.close();
+  } catch (error) {
+    throw new Error('There was a problem closing the database connection:', error);
+  }
   
   // Set db instance to null
-
+  dbInstance = null;
+  currentDbFilepath = null;
 }
 
 // Given a database filepath, delete the database
-async function deleteDb(dbFilepath) {
-  // Check that the db exists
+export async function deleteDb(dbFilepath) {
+  
+  // Check to make sure db exists
+  try {
+    await fs.access(dbFilepath);
+  } catch (error) {
+    throw new Error(`Could not find database ${dbFilepath}:`, error);
+  }
 
+  // Ensure that the db to be deleted is the current connection
+  if (dbFilepath == currentDbFilepath) {
+    throw new Error(`Cannot delete database at ${dbFilepath} that is the current connection.`)
+  }
+
+  // Try to delete the file
+  try {
+    await fs.rm(dbFilepath);
+  } catch(error) {
+    throw new Error(`Could not delete database at ${dbFilepath}`, error);
+  }
 }
 
 // Get database instance
-async function getDbInstance() {
+export async function getDbInstance() {
   // Ensure there is currently a db instance
+  if (!dbInstance & !currentDbFilepath) {
+    throw new Error('There is no database instance');
+  }
   
   // Return the db instance
-
+  return dbInstance;
 }
+
