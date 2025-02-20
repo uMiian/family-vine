b7c81265ee97c9107f345b6acba966773600e79c
moo20222 <samuel@ou.edu>
2025-02-20 16:44:52 -0600

    readability improvements

--- src/main/handlers/databaseHandler.js
+++ src/main/handlers/databaseHandler.js
@@ -22,9 +22,6 @@ const createTables = () => {
 createTables();
 
 db.close((err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Closed the database connection.');
-    }
+    if (err) console.error(err.message);
+    else console.log('Closed the database connection.');
 });
